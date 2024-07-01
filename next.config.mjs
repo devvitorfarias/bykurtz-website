0348f41b244a950edafec6817d49db02d030b829
dev.vitor <contato.vitorfariasss@gmail.com>
2024-07-01 19:44:07 -0300

    main

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,8 @@ const nextConfig = {
     ],
     unoptimized: true, 
   },
+  
+  trailingSlash: true, 
   output: 'export'
 };
 
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -4,10 +4,12 @@ const Portfolio = () => {
     return (
         <>
             <div className="w-full  bg-background border-b border-black">
-                <div className="px-6">
-                    <div>
-                        <Image width={300} height={40} src="https://firebasestorage.googleapis.com/v0/b/bykurtz-website.appspot.com/o/motorola-logo.png?alt=media" alt="logo" />
-                        <div>
+                <div className="px-6 py-6">
+                    <div className="p-6">
+                        <div className="my-6">
+                            <Image width={300} height={40} src="https://firebasestorage.googleapis.com/v0/b/bykurtz-website.appspot.com/o/motorola-logo.png?alt=media" alt="logo" />
+                        </div>
+                        <div className="p-6">
                             <h1 className="text-white font-semibold">Challenge:</h1>
                             <span className="text-white font-medium">
                                 Motorola needed a partner to develop personalized solutions focused on
@@ -18,7 +20,7 @@ const Portfolio = () => {
                                 Bykurtz provided design adjustments, reduced bugs through proactive testing, and
                                 integrated product recommendations based on customer behavior
                             </span>
-                            <div>
+                            <div className="p-6">
                                 <h1 className="text-white font-semibold">Results:</h1>
                                 <li className="text-white font-medium">Increased customer engagement and satisfaction.</li>
                                 <li className="text-white font-medium">Reduced technical issues and improved site performance</li>
@@ -28,8 +30,10 @@ const Portfolio = () => {
                                 asset to our team."</span>
                         </div>
                     </div>
-                    <Image width={300} height={40} src="https://firebasestorage.googleapis.com/v0/b/bykurtz-website.appspot.com/o/logo-shoulder.png?alt=media" alt="logo" />
-                    <div>
+                    <div className="my-6">
+                        <Image width={300} height={40} src="https://firebasestorage.googleapis.com/v0/b/bykurtz-website.appspot.com/o/logo-shoulder.png?alt=media" alt="logo" />
+                    </div>
+                    <div className="p-6">
                         <span>
                             <h1 className="text-white font-semibold">Challenge:</h1>
                             <span className="text-white font-medium">Shoulder aimed to improve their customer experience and keep up with eCommerce
@@ -40,7 +44,7 @@ const Portfolio = () => {
                                 speed, and ensured compatibility across various devices and browsers.
                             </span>
                         </span>
-                        <div>
+                        <div className="p-6">
                             <h1 className="text-white font-semibold">Results:</h1>
                             <li className="text-white font-medium">50% increase in online sales.</li>
                             <li className="text-white font-medium">20% reduction in technical issues affecting user experience.</li>
@@ -50,9 +54,39 @@ const Portfolio = () => {
                             "The new e-commerce platform developed by Bykurtz revolutionized our online business."
                         </span>
                     </div>
-                    <Image width={300} height={40} src="https://firebasestorage.googleapis.com/v0/b/bykurtz-website.appspot.com/o/slogo-samsung.png?alt=media" alt="logo" />
-                    <Image width={300} height={40} src="https://firebasestorage.googleapis.com/v0/b/bykurtz-website.appspot.com/o/Carrefour-logo.png?alt=media" alt="logo" />
-                    <div>
+                    <div className="my-6">
+                        <Image width={300} height={40} src="https://firebasestorage.googleapis.com/v0/b/bykurtz-website.appspot.com/o/slogo-samsung.png?alt=media" alt="logo" />
+                    </div>
+                    <div className="p-6">
+                        <span>
+                            <h1 className="text-white font-semibold">Challenge:</h1>
+                            <span className="text-white font-medium">Samsung required a redesigned online platform to improve navigability and user
+                                experience, along with integrating new functionalities to boost operational efficiency.
+                                Solution: Bykurtz redesigned the platform, creating intuitive and visually appealing interfaces,
+                                and implemented new features to enhance functionality.
+
+                            </span>
+                            <h1 className="text-white font-semibold">Solution: </h1>
+                            <span className="text-white font-medium">
+                                Bykurtz developed a tailored software solution that integrated seamlessly with
+                                Carrefour’s existing systems, providing comprehensive control over all internal movements.
+                            </span>
+                        </span>
+                        <div>
+                            <h1 className="text-white font-semibold">Results:</h1>
+                            <li className="text-white font-medium">30% increase in operational efficiency.</li>
+                            <li className="text-white font-medium">Improved user experience leading to higher customer satisfaction</li>
+                            <li className="text-white font-medium">Enhanced platform navigability.</li>
+                        </div>
+                        <span className="text-white font-medium">
+                            "Bykurtz's exceptional technical knowledge and problem-solving abilities significantly improved our online
+                            platform and operational efficiency."
+                        </span>
+                    </div>
+                    <div className="my-6">
+                        <Image width={300} height={40} src="https://firebasestorage.googleapis.com/v0/b/bykurtz-website.appspot.com/o/Carrefour-logo.png?alt=media" alt="logo" />
+                    </div>
+                    <div className="p-6">
                         <span>
                             <h1 className="text-white font-semibold">Challenge:</h1>
                             <span className="text-white font-medium">Carrefour needed to improve control over their internal operations and sought a
@@ -75,8 +109,10 @@ const Portfolio = () => {
                             control over all company operations."
                         </span>
                     </div>
-                    <Image width={300} height={40} src="https://firebasestorage.googleapis.com/v0/b/bykurtz-website.appspot.com/o/whirlpool-logo.png?alt=media" alt="logo" />
-                    <div>
+                    <div className="my-6">
+                        <Image width={300} height={40} src="https://firebasestorage.googleapis.com/v0/b/bykurtz-website.appspot.com/o/whirlpool-logo.png?alt=media" alt="logo" />
+                    </div>
+                    <div className="p-6">
                         <h1 className="text-white font-semibold">Challenge:</h1>
                         <span className="text-white font-medium">Carrefour needed to improve control over their internal operations and sought a
                             custom software solution to enhance their financial, operational, and tax processes.
